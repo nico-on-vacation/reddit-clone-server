dd7b899f079f069bffc2cd5f00b733d49170e97c
Nico <56159077+nico-on-vacation@users.noreply.github.com>
2021-02-17 21:21:08 +0100

    Delete dist directory

--- dist/utils/sleep.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-const sleep = (t) => new Promise((res) => setTimeout(res, t));
-exports.sleep = sleep;
-//# sourceMappingURL=sleep.js.map
